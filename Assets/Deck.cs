180fd06d00a787b8f2e0e066ed5cc0763a325b6f
Adam Coyle <adam.coyle@fhnw.ch>
2018-10-30 13:49:39 +0100

    Added a Deck Script which generates children Card Prefabs with an array of CardData objects.

---
+++ Assets/Deck.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TwentyOneRemastered
+{
+    public class Deck : MonoBehaviour
+    {
+
+        const int DECK_SIZE = 10;
+
+        [SerializeField]
+        GameObject cardPrefab;
+
+        [SerializeField]
+        CardData[] cardDataArray;
+
+        #region UNITY LIFECYCLE
+
+        void Start()
+        {
+            GenerateDeck();
+        } 
+
+        #endregion
+
+        #region PRIVATE FUNCTIONS
+
+        void GenerateDeck()
+        {
+            foreach (CardData data in cardDataArray)
+            {
+                cardPrefab.GetComponent<Card>().cardData = data;
+                GameObject newCard = Instantiate(cardPrefab);
+                newCard.transform.parent = transform;
+                newCard.name = data.name;
+            }
+        }
+
+        #endregion
+    } 
+}
