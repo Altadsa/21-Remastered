218a259387075b416f453807116a53d2d12e9a42
Adam Coyle <adam.coyle@fhnw.ch>
2018-10-30 15:23:08 +0100

    Cards now Move to the Location of the Player hand in a smooth fashion.

--- Assets/PlayerHand.cs
+++ Assets/PlayerHand.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace TwentyOneRemastered
 {
@@ -7,11 +8,7 @@ namespace TwentyOneRemastered
 
         int handValue;
 
-        // Use this for initialization
-        void Start()
-        {
-
-        }
+        float padding = 0.2f;
 
         // Update is called once per frame
         void Update()
@@ -19,12 +16,39 @@ namespace TwentyOneRemastered
 
             if (Input.GetKeyDown(KeyCode.A) && Deck.Instance.transform.childCount > 0)
             {
-                Deck.Instance.transform.GetChild(0).transform.position = transform.position;
-                handValue += Deck.Instance.transform.GetChild(0).GetComponent<Card>().cardData.Value;
-                Deck.Instance.transform.GetChild(0).transform.parent = transform;
+                AddCardToHand();
+                SetOrderInLayer();
                 Debug.Log(handValue);
             }
+            foreach (Transform child in transform)
+            {
+                AdjustCards(child);
+            }
+        }
 
+        private void AddCardToHand()
+        {
+            Transform deckCard = Deck.Instance.transform.GetChild(0);
+            deckCard.transform.position = Vector3.Lerp(deckCard.transform.position, transform.position, 1.0f * Time.deltaTime);
+            handValue += deckCard.GetComponent<Card>().cardData.Value;
+            deckCard.transform.parent = transform;
+        }
+
+        private void AdjustCards(Transform child)
+        {
+            Vector2 childPos = child.position;
+            Vector2 newPos = new Vector2(transform.position.x + padding, transform.position.y);
+            child.position = Vector3.Lerp(child.position, newPos, 2.0f * Time.deltaTime);
+        }
+
+        private void SetOrderInLayer()
+        {
+            int order = 0;
+            foreach (Transform child in transform)
+            {
+                child.GetComponent<SpriteRenderer>().sortingOrder = order;
+                order++;
+            }
         }
     } 
 }
