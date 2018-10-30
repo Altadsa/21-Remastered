3a2bdf8e2f5480a6cb2689e1d4b3aa20f6eb349a
Adam Coyle <adam.coyle@fhnw.ch>
2018-10-30 13:33:04 +0100

    Added a default sprite (back of card). Created the Card Script which utilizes a CardData object.

---
+++ Assets/Card.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TwentyOneRemastered
+{
+    public class Card : MonoBehaviour
+    {
+        [SerializeField]
+        Sprite defaultSprite;
+
+        public CardData cardData;
+
+        string cardName;
+        int cardValue;
+        CardSuit suit;
+
+        // Use this for initialization
+        void Start()
+        {
+            Initialize();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        #region PRIVATE FUNCTIONS
+
+        void Initialize()
+        {
+            cardName = cardData.CardName;
+            cardValue = cardData.Value;
+            suit = cardData.CardSuit;
+            if (cardData.CardSprite)
+            {
+                GetComponent<SpriteRenderer>().sprite = cardData.CardSprite;
+                return;
+            }
+            GetComponent<SpriteRenderer>().sprite = defaultSprite;
+        }
+
+        #endregion
+    } 
+}
