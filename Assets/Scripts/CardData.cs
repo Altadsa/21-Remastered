d8d6ccb30d715504e1c9dc18088d7001b5c5ed8d
Adam Coyle <adam.coyle@fhnw.ch>
2018-10-30 11:39:00 +0100

    Added CardData Scriptable Objects that will contain Card Data.

---
+++ Assets/Scripts/CardData.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace TwentyOneRemastered
+{
+    public enum CardSuit
+    {
+        CLUBS,
+        DIAMONDS,
+        HEARTS,
+        SPADES
+    }
+
+    [CreateAssetMenu(menuName = "21/Card Data")]
+    public class CardData : ScriptableObject
+    {
+
+        #region VALUES
+
+        [SerializeField]
+        string cardName;
+
+        [SerializeField]
+        int value;
+
+        [SerializeField]
+        Sprite cardSprite;
+
+        [SerializeField]
+        CardSuit cardSuit;
+
+        #endregion
+
+        #region PROPERTIES
+
+        public string CardName { get { return cardName; } }
+        public int Value { get { return value; } }
+        public Sprite CardSprite { get { return cardSprite; } } 
+
+        #endregion
+
+    } 
+}
