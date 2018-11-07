662ed3d90f47856a5064683008e5ce2cd8f6f8d3
Adam Coyle <adam.coyle@fhnw.ch>
2018-11-07 13:58:26 +0100

    Added Functionality to the Rules Menu.

---
+++ Assets/Scripts/StringConstant.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace TwentyOneRemastered
+{
+    [CreateAssetMenu(menuName = "21/Constants/String")]
+    public class StringConstant : ScriptableObject
+    {
+        [TextArea(1, 5)]
+        [SerializeField]
+        string value;
+
+        public string Value { get { return value; } }
+
+    } 
+}
